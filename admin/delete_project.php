4cfe665e618701257d2915919414ea8bbabf00bf
krishantha lakdilu <krishanlakdilu12345@gmail.com>
2025-08-17 11:34:30 -0700

    Delete admin directory

--- admin/delete_project.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . '/../includes/auth.php';
-ensure_auth();
-require __DIR__ . '/../includes/functions.php';
-
-$id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
-if ($id) { deleteClient($id); }
-header("Location: /final_project_tracker_sql/admin/dashboard.php");
-exit;
--- admin/logout.php
+++
@@ -1,3 +0,0 @@
-<?php
-require __DIR__ . '/../includes/auth.php';
-logout();
